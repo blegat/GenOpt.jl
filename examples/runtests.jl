7cfb319c01b9eacea18557421a5a3339000fd4f9
Benoît Legat <benoit.legat@gmail.com>
2026-09-21 08:57:14 +0200

    Use _include_sandbox

--- examples/runtests.jl
+++ examples/runtests.jl
@@ -1,9 +1,22 @@
 using Test
 
+"""
+    _include_sandbox(filename)
+
+Include the `filename` in a temporary module that acts as a sandbox. (Ensuring
+no constants or functions leak into other files.)
+
+This function was taken from `JuMP/docs/make.jl`.
+"""
+function _include_sandbox(filename)
+    mod = @eval module $(gensym()) end
+    return Base.include(mod, filename)
+end
+
 for dir in readdir(@__DIR__)
     if isdir(joinpath(@__DIR__, dir))
         @testset "$dir" begin
-            include(joinpath(@__DIR__, dir, "runtests.jl"))
+            _include_sandbox(joinpath(@__DIR__, dir, "runtests.jl"))
         end
     end
 end
